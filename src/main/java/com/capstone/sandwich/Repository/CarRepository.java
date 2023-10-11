610ddae358788357895c00fc0436947149aea840
kim <justinkim1201@gmail.com>
2023-10-11 17:30:00 +0900

    refactor: 클래스명 수정
    DTO 및 Repository 클래스명 수정

--- src/main/java/com/capstone/sandwich/Repository/CarRepository.java
+++ src/main/java/com/capstone/sandwich/Repository/CarRepository.java
@@ -3,5 +3,5 @@ package com.capstone.sandwich.Repository;
 import com.capstone.sandwich.Domain.Entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface carRepository extends JpaRepository<Car,Integer> {
+public interface CarRepository extends JpaRepository<Car,Integer> {
 }
