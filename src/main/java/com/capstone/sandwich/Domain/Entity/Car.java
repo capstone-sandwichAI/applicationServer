3c2c2f18d7f3028d29378527d42066dabf3c7d64
ABCganada <shinmg9712@gmail.com>
2023-10-25 21:35:20 +0900

    feat: yml파일 & mysql 연동

--- src/main/java/com/capstone/sandwich/Domain/Entity/Car.java
+++ src/main/java/com/capstone/sandwich/Domain/Entity/Car.java
@@ -1,19 +1,21 @@
 package com.capstone.sandwich.Domain.Entity;
 
-import lombok.Generated;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Builder
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Car {
 
-    @Id @Generated
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String carNumber;
