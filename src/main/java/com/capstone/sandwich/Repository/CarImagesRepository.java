85b2b958fd7f0705f03282f2d84c845dc0e642cd
ABCganada <shinmg9712@gmail.com>
2023-10-26 20:51:52 +0900

    feat: car_images 엔티티 추가

---
+++ src/main/java/com/capstone/sandwich/Repository/CarImagesRepository.java
@@ -0,0 +1,8 @@
+package com.capstone.sandwich.Repository;
+
+import com.capstone.sandwich.Domain.Entity.CarImages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarImagesRepository extends JpaRepository<CarImages, Integer> {
+
+}
