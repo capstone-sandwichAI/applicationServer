e8233c93daa2ba1c60252f73e5bbe474a66d848c
kim <justinkim1201@gmail.com>
2023-10-27 14:31:04 +0900

    refactor: DTO default값 제거, @AllArgsConstructor 추가
    BackResponseDTO report setter 추가

--- src/main/java/com/capstone/sandwich/Domain/DTO/BackResponseDTO.java
+++ src/main/java/com/capstone/sandwich/Domain/DTO/BackResponseDTO.java
@@ -1,23 +1,29 @@
 package com.capstone.sandwich.Domain.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class BackResponseDTO {
 
     private String carNumber;
     private MultipartFile resultVideo;
 
-    private Integer scratch=0; //스크래치 개수
-    private Integer installation=0; // 장착 불량 개수
-    private Integer exterior=0; //외관 손상 개수
-    private Integer gap=0; // 단차 손상 개수
+    private Integer scratch; //스크래치 개수
+    private Integer installation; // 장착 불량 개수
+    private Integer exterior; //외관 손상 개수
+    private Integer gap; // 단차 손상 개수
 
-    private Integer totalDefects=0;
+    private Integer totalDefects;
 
     //자동 레포트
     private String report;
+
+    public void setReport(String report) {
+        this.report = report;
+    }
 }
--- src/main/java/com/capstone/sandwich/Domain/DTO/RequestDTO.java
+++ src/main/java/com/capstone/sandwich/Domain/DTO/RequestDTO.java
@@ -1,6 +1,7 @@
 package com.capstone.sandwich.Domain.DTO;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RequestDTO {
 
     private String carNumber;
