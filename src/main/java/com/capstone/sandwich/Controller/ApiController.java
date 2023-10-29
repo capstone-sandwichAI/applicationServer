3803b1df5a6fff13a494eb86adee4c07a893bed6
kim <justinkim1201@gmail.com>
2023-10-29 16:35:30 +0900

    feat: carService, ExceptionController 추가
    carService, carServiceTest 추가
    테스트용 사진 추가
    Exception 추가
    build.gradle에 테스트용 h2 추가

--- src/main/java/com/capstone/sandwich/Controller/ApiController.java
+++ src/main/java/com/capstone/sandwich/Controller/ApiController.java
@@ -1,6 +1,8 @@
 package com.capstone.sandwich.Controller;
 
 import com.capstone.sandwich.Domain.DTO.RequestDTO;
+import com.capstone.sandwich.Domain.Exception.ApiException;
+import com.capstone.sandwich.Service.CarService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +14,16 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class ApiController {
 
-//    private final CarService
+    private final CarService carService;
 
     @PostMapping("/inspection/{carNumber}")
-    public void frontRequest(@ModelAttribute RequestDTO requestDTO, @PathVariable("carNumber") String carNumber) {
+
+    public void frontRequest(@ModelAttribute RequestDTO requestDTO, @PathVariable("carNumber") String carNumber) throws ApiException {
         requestDTO.setCarNumber(carNumber);
-        log.info("request car = {}, image cnt = {}",requestDTO.getCarNumber(),requestDTO.getPhotos().size());
+        log.info("request car = {}, image cnt = {}", requestDTO.getCarNumber(), requestDTO.getPhotos().size());
 
         //validation
-
+        carService.validateDTO(requestDTO);
         //request to Ai
 
         //response from Ai
@@ -28,7 +31,6 @@ public class ApiController {
         //insert DB
 
         //make Report
-
 
 
     }
---
+++ src/main/java/com/capstone/sandwich/Service/CarService.java
@@ -0,0 +1,41 @@
+package com.capstone.sandwich.Service;
+
+import com.capstone.sandwich.Domain.DTO.RequestDTO;
+import com.capstone.sandwich.Domain.Exception.ApiException;
+import com.capstone.sandwich.Repository.CarImagesRepository;
+import com.capstone.sandwich.Repository.CarRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CarService {
+
+//    private final CarRepository carRepository;
+//    private final CarImagesRepository carImagesRepository;
+
+    public Integer validateDTO(RequestDTO dto) throws ApiException {
+        log.info("Validation start");
+        List<MultipartFile> photos = dto.getPhotos();
+        log.info("photo size ={}",photos.size());
+        if(photos.get(0).isEmpty())
+            throw new ApiException(HttpStatus.NOT_FOUND,ApiException.NullException());
+        List<MultipartFile> unsupportedFiles = photos.stream().filter(
+                file -> (!file.getContentType().equals("image/png")) //jpeg도 포함 0
+        ).collect(Collectors.toList());
+
+        if (unsupportedFiles.size() > 0)
+            throw new ApiException(HttpStatus.UNSUPPORTED_MEDIA_TYPE,ApiException.MediaTypeException(unsupportedFiles.toString()));
+
+        log.info("Validation Success");
+        return unsupportedFiles.size();
+    }
+
+}
