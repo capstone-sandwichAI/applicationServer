a504040bb6f52352676a9bf1606518a6896a9f2b
ABCganada <shinmg9712@gmail.com>
2023-11-04 00:02:39 +0900

    test: 이미지 + 삭제 테스트 코드 작성

--- src/test/java/com/capstone/sandwich/aws/s3/service/S3ServiceTest.java
+++ src/test/java/com/capstone/sandwich/aws/s3/service/S3ServiceTest.java
@@ -7,6 +7,8 @@ import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class S3ServiceTest {
 
@@ -14,12 +16,16 @@ public class S3ServiceTest {
     private S3Service s3Service;
 
     @Test
-    public void S3_버킷에_이미지_업로드() throws IOException {
+    public void S3_버킷에_이미지_업로드_및_삭제() throws IOException {
 
         MockMultipartFile file = new MockMultipartFile("image", "image.jpg", "image/jpg", "image content".getBytes());
 
         String imageUrl = s3Service.upload(file);
 
         assert imageUrl != null && !imageUrl.isEmpty();
+
+        s3Service.delete(imageUrl);
+
+        assertThat(s3Service.doesObjectExist(imageUrl)).isFalse();
     }
 }
