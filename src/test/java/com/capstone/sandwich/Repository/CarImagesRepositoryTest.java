913983635ad741f78ff92b7705cb458cd4f88641
ABCganada <shinmg9712@gmail.com>
2023-11-09 23:40:44 +0900

    test: 이미지 삽입 조회 로직 테스트 추가

--- src/test/java/com/capstone/sandwich/Repository/CarImagesRepositoryTest.java
+++ src/test/java/com/capstone/sandwich/Repository/CarImagesRepositoryTest.java
@@ -10,12 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Transactional
 @SpringBootTest
 public class CarImagesRepositoryTest {
 
@@ -38,7 +40,6 @@ public class CarImagesRepositoryTest {
     }
 
     @Test
-    @Transactional
     public void 차량_사진_레포_데이터_삽입_조회(){
         //given
         Car car = Car.builder()
@@ -79,5 +80,46 @@ public class CarImagesRepositoryTest {
         } else{
             assertFalse(false);
         }
+    }
+
+    @Test
+    public void 차량_엔티티_및_이미지_삽입_조회(){
+        //given
+        Car car = Car.builder()
+                .carNumber("a1b2c3")
+                .gap(1)
+                .scratch(2)
+                .installation(0)
+                .exterior(0)
+                .totalDefects(3)
+                .createdDate(LocalDate.now())
+                .build();
+
+        List<CarImages> carImages = new ArrayList<>();
+
+        for (int i = 1; i <= 8; i++) {
+            CarImages carImage =  CarImages.builder()
+                    .car(car)
+                    .imageUrl("https://sandwich-s3-bucket.s3.ap-northeast-2.amazonaws.com/test-image-" + i + ".jpeg")
+                    .build();
+
+            carImages.add(carImage);
+        }
+
+        carRepository.save(car);
+        carImagesRepository.saveAll(carImages);
+
+        //when
+        List<Car> carList = carRepository.findAll();
+        Car findCar = carList.get(0);
+
+        List<CarImages> carImagesList = carImagesRepository.findAll();
+
+        //then
+        assertThat(findCar.getCarNumber()).isEqualTo(car.getCarNumber());
+        assertThat(findCar.getCreatedDate()).isEqualTo(car.getCreatedDate());
+
+        assertThat(carImagesList).isNotNull();
+        assertThat(carImagesList).containsExactlyInAnyOrderElementsOf(carImages);
     }
 }
